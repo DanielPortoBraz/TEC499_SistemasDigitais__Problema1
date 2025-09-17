08186c128fc9190ee2ad6092657467621f32ed43
DisneyCarvalho <114960349+DisneyCarvalho@users.noreply.github.com>
2025-09-17 16:41:50 -0300

    Add files via upload

--- meta_3.v
+++ meta_3.v
@@ -27,8 +27,29 @@ module meta_3(
     wire [16:0] address;
     wire [7:0]  pixel_data;
     wire [9:0]  img_x, img_y;
+	 
+	 
+	 
+	 // uco
+	 wire [18:0] addr_from_vga_calc;
+    wire [16:0] src_addr_from_cpu;
+    wire [18:0] dest_addr_from_cpu;
+    wire [7:0]  data_from_cpu;
+    wire        wren_from_cpu;
+	 
+	 
+	 
+	 // memoria
+	 wire [16:0] src_mem_addr;
+    wire [18:0] dest_mem_addr;
+    wire [7:0]  src_mem_data_out;
+    wire [7:0]  dest_mem_data_in;
+    wire        dest_mem_wren;
+	 
+	 
 
     // ==== CPU (com ULA integrada) ====
+	 
     cpu u_cpu (
         .clk_in(clock_25mhz),
         .next_x(next_x),
@@ -39,21 +60,15 @@ module meta_3(
         .address(address)
     );
 
-    // ==== Memória de imagem ====
-    memory2 u_mem (
-        .clock(clock),
-        .data(8'd0),
-        .address(address),
-        .wren(1'b0),
-        .q(pixel_data)
-    );
-
+    
+	 
     // ==== Controlador VGA ====
     vga_module u_vga (
         .clock(clock_25mhz),
         .color_in(pixel_data), // envia pixel lido
         .next_x(next_x),
         .next_y(next_y),
+		  
         .hsync(hsync),
         .vsync(vsync),
         .red(red),
@@ -63,6 +78,55 @@ module meta_3(
         .clk(clk),
         .blank(blank)
     );
+	 
+	 
+	 
+	 
+	 // ==== Memória de falsa ====
+	 memory2 f_mem (
+        .clock(clock),
+        .data(8'd0),
+        .address(address_fal),
+        .wren(1'b0),
+        .q(pixel_data)
+    );
+	 
+	 
+	 
+	 // ==== Memória de original ROM 320x240
+    original a_mem (
+        .clock(clock),
+        .address(address_ori),
+        .q()
+    );
+	 
+	 
+	 
+	 
+	 
+	 uc u_uc (
+        .clk(clock_25mhz),
+        .reset(reset),
+        .chaves(chaves),
+        .botao_aplicar(botao_aplicar),
+		  
+        .addr_from_vga_calc(addr_from_vga_calc),
+		  
+		  .cpu_start(cpu_start), //Cpu
+        .cpu_done(cpu_done),
+		  .sistema_ocupado(sistema_ocupado),
+        .src_addr_from_cpu(src_addr_from_cpu), 
+        .dest_addr_from_cpu(dest_addr_from_cpu),
+        .data_from_cpu(data_from_cpu),
+        .wren_from_cpu(wren_from_cpu),
+		  
+        .src_mem_addr(address_ori), //Addr vem da cpu
+        .dest_mem_addr(address_fal), //Addr da mem de exibição
+		  
+        .dest_mem_data(dest_mem_data_in), //Habilita escrita
+        .dest_mem_wren(dest_mem_wren)     //Dado da escrita
+    );
+	 
 
 endmodule
 
--- uc.v
+++ uc.v
@@ -1,41 +1,82 @@
+
+
+
 module uc (
+    
     input  wire        clk,
-    input  wire [7:0]  pc,
-    input  wire [7:0]  pixel_in,
-    input  wire [9:0]  img_x_in,   // vindo da ULA
-    input  wire [9:0]  img_y_in,   // vindo da ULA
-    output reg  [9:0]  next_x,
-    output reg  [9:0]  next_y,
-    output reg  [2:0]  ch,
-    output reg  [16:0] address,
-    output reg  [9:0]  img_x,
-    output reg  [9:0]  img_y,
-    output reg  [7:0]  pixel_out
+    input  wire        reset,
+    input  wire [2:0]  chaves,          
+    input  wire        botao_aplicar,     
+    input  wire        cpu_done,          
+    
+    output reg         sistema_ocupado,   
+    
+    output reg         cpu_start,
+    
+    input  wire [18:0] addr_from_vga_calc,
+
+    input  wire [16:0] src_addr_from_cpu,
+    input  wire [18:0] dest_addr_from_cpu,
+    input  wire [7:0]  data_from_cpu,
+    input  wire        wren_from_cpu,
+
+    output reg [16:0]  src_mem_addr,
+    output reg [18:0]  dest_mem_addr,
+    output reg [7:0]   dest_mem_data,
+    output reg         dest_mem_wren
 );
 
-    // Instruction memory (ROM simples)
+    
+    localparam S_IDLE              = 2'd0; 
+    localparam S_INICIAR_PROCESSO  = 2'd1; 
+    localparam S_AGUARDANDO_CPU    = 2'd2; 
+
+    reg [1:0] state, next_state;
+
+    
+    always @(posedge clk or posedge reset) begin
+        if (reset) state <= S_IDLE;
+        else       state <= next_state;
+    end
+	 
+	 
+
     always @(*) begin
-        case (pc)
-            8'd0: begin next_x = 10'd50;  next_y = 10'd60;  ch = 3'b000; end
-            8'd1: begin next_x = 10'd100; next_y = 10'd80;  ch = 3'b010; end
-            8'd2: begin next_x = 10'd150; next_y = 10'd120; ch = 3'b100; end
-            default: begin next_x = 10'd0; next_y = 10'd0;  ch = 3'b000; end
+        
+        next_state = state;
+        cpu_start = 1'b0;
+        sistema_ocupado = (state != S_IDLE);
+
+        src_mem_addr = 0; 
+        dest_mem_addr = addr_from_vga_calc; 
+        dest_mem_data = 8'd0;
+        dest_mem_wren = 1'b0;
+
+		  
+        case(state)
+            S_IDLE: begin
+                if (botao_aplicar) begin
+                    next_state = S_INICIAR_PROCESSO;
+                end
+            end
+            
+            S_INICIAR_PROCESSO: begin
+                cpu_start = 1'b1; 
+                next_state = S_AGUARDANDO_CPU;
+            end
+
+            S_AGUARDANDO_CPU: begin
+                
+                src_mem_addr = src_addr_from_cpu;
+                dest_mem_addr = dest_addr_from_cpu;
+                dest_mem_data = data_from_cpu;
+                dest_mem_wren = wren_from_cpu;
+
+                if (cpu_done) begin
+                    next_state = S_IDLE; 
+                end
+            end
         endcase
-    end
-
-    // Cálculo do endereço
-    always @(*) begin
-        if (img_x_in < 320 && img_y_in < 240)
-            address = img_y_in * 320 + img_x_in;
-        else
-            address = 0;
-    end
-
-    // Register bank (armazenamento sincronizado)
-    always @(posedge clk) begin
-        img_x     <= img_x_in;
-        img_y     <= img_y_in;
-        pixel_out <= pixel_in;
     end
 
 endmodule
